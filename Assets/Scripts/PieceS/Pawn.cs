da770898e570acb2bd8219b7dbd60fbf906a80fb
Zach <78343206+ZariiAU@users.noreply.github.com>
2024-04-16 17:49:52 +0930

    Implemented Pawn's attack move criteria
    Fixed out of range bug when the piece checks for attack moves past the grid bounds.
    
    Added check for legal moves when attacking to prevent pawns being able to teleport across the board to any opposing piece

--- Assets/Scripts/PieceS/Pawn.cs
+++ Assets/Scripts/PieceS/Pawn.cs
@@ -8,27 +8,56 @@ public class Pawn : Piece
     {
         List<Tile> list = new List<Tile>();
 
-        //// North of Piece
+        //// North of Piece if on white team
         if(allegiance == gridObject.GetComponent<PieceSpawner>().whiteData)
         {
-            for (int i = (int)currentTile.gridPos.y + 1; i < (int)currentTile.gridPos.y + 2; i++)
+            for (int i = (int)currentTile.gridPos.y + 1; i < (int)currentTile.gridPos.y + 2 && i < gridObject.gridDimensions.GetLength(1); i++)
             {
                 Tile nextTile = gridObject.gridDimensions[(int)currentTile.gridPos.x, i];
                 if (nextTile.pieceOnTile)
                 {
                     Piece nextPiece = nextTile.pieceOnTile;
-                    if (nextPiece && allegiance == nextPiece.allegiance)
+                    if (nextPiece)
                     {
                         break;
-                    }
-                    else
-                    {
-                        // ATTACK!!!
                     }
                 }
                 list.Add(nextTile);
             }
+
+            if(currentTile.gridPos.y < gridObject.gridDimensions.GetLength(1) - 1) // Don't check anything above us if we're near the north edge of the grid
+            {
+                // Don't check NW if we're at the min x bounds
+                if (currentTile.gridPos.x > 0) 
+                {
+                    Tile attackTileNW = gridObject.gridDimensions[(int)currentTile.gridPos.x - 1, (int)currentTile.gridPos.y + 1];
+                    if (attackTileNW.pieceOnTile)
+                    {
+                        Piece nextPiece = attackTileNW.pieceOnTile;
+                        if (nextPiece && allegiance != nextPiece.allegiance)
+                        {
+                            list.Add(attackTileNW);
+                        }
+                    }
+                }
+
+                // Don't check NE if we're at the max x bounds
+                if (currentTile.gridPos.x < gridObject.gridDimensions.GetLength(0) - 1) 
+                {
+                    Tile attackTileNE = gridObject.gridDimensions[(int)currentTile.gridPos.x + 1, (int)currentTile.gridPos.y + 1];
+                    if (attackTileNE.pieceOnTile)
+                    {
+                        Piece nextPiece = attackTileNE.pieceOnTile;
+                        if (nextPiece && allegiance != nextPiece.allegiance)
+                        {
+                            list.Add(attackTileNE);
+                        }
+                    }
+                }
+            }
         }
+
+        // South of piece if on anything else
         else
         {
             for (int i = (int)currentTile.gridPos.y - 1; i >= 0 && i > (int)currentTile.gridPos.y - 2; i--)
@@ -37,16 +66,41 @@ public class Pawn : Piece
                 if (nextTile.pieceOnTile)
                 {
                     Piece nextPiece = nextTile.pieceOnTile;
-                    if (nextPiece && allegiance == nextPiece.allegiance)
+                    if (nextPiece)
                     {
                         break;
                     }
-                    else
-                    {
-                        // ATTACK!!!
-                    }
                 }
                 list.Add(nextTile);
+            }
+
+            if (currentTile.gridPos.y > 0)
+            {
+                if(currentTile.gridPos.x > 0)
+                {
+                    Tile attackTileSW = gridObject.gridDimensions[(int)currentTile.gridPos.x - 1, (int)currentTile.gridPos.y - 1];
+                    if (attackTileSW.pieceOnTile)
+                    {
+                        Piece nextPiece = attackTileSW.pieceOnTile;
+                        if (nextPiece && allegiance != nextPiece.allegiance)
+                        {
+                            list.Add(attackTileSW);
+                        }
+                    }
+                }
+                
+                if(currentTile.gridPos.x < gridObject.gridDimensions.GetLength(0) - 1)
+                {
+                    Tile attackTileSE = gridObject.gridDimensions[(int)currentTile.gridPos.x + 1, (int)currentTile.gridPos.y - 1];
+                    if (attackTileSE.pieceOnTile)
+                    {
+                        Piece nextPiece = attackTileSE.pieceOnTile;
+                        if (nextPiece && allegiance != nextPiece.allegiance)
+                        {
+                            list.Add(attackTileSE);
+                        }
+                    }
+                }
             }
         }
 
